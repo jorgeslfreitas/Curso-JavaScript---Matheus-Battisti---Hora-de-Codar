c8c051bb5c920737dfc01dee5dabb6b8bd6aeb57
jorgeslfreitas <jorgeslfreitas@gmail.com>
2023-05-19 06:59:18 -0300

    Aula 22
    Aula 22 - Funções

---
+++ 22_funcoes/js/script.js
@@ -0,0 +1,20 @@
+function primeiraFuncao() {
+    console.log(`Primeiro uso de função no curso!`);
+}
+
+primeiraFuncao();
+
+function segundaFuncao(x, y) {
+    return x + y;
+}
+
+let soma = segundaFuncao(5, 10);
+console.log(soma);
+
+function multiplica(b, c) {
+    let multipliquei = b * c;
+    return multipliquei;
+}
+
+let result = multiplica(10, 5);
+console.log(result);
